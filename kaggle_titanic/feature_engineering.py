164e0ff0692d915d52488446a54c73574ebe1a0f
yuskey <heredia.brian@gmail.com>
2018-07-15 14:51:01 -0400

    Added feature engineering
    Removed multiple datasets and added feature engineering.py

---
+++ kaggle_titanic/feature_engineering.py
@@ -0,0 +1,35 @@
+﻿import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+
+
+train = pd.read_csv('train.csv', header = 0)
+test = pd.read_csv('test.csv', header = 0)
+test['Survived'] = 0
+
+#combine test and training to make feature changes to both datasets
+concat = train.append(test, ignore_index=True)
+
+print(train.shape)  #891 instances
+print(test.shape)   #418 instances
+print(concat.shape)
+
+
+#train['Embarked'] = train['Embarked'].fillna('A')
+
+#convert one-hot features like sex and embarked
+#encoder = preprocessing.LabelBinarizer()
+#new_sex = encoder.fit_transform(train['Sex'])
+#new_embarked = encoder.fit_transform(train['Embarked'])
+#train['Sex'] = new_sex
+#train['Embarked'] = new_embarked
+
+#clf = LogisticRegression()
+#clf.fit(X_train, y_train)
+#scores = cross_val_score(clf, X_train, y_train, cv=5)
+#print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+#clf.predict(X_test)
--- kaggle_titanic/titanic_data_processing.py
+++ kaggle_titanic/titanic_data_processing.py
@@ -3,13 +3,19 @@ import csv
 import scipy as sp
 import numpy as np
 
-#reads in the training csv to a list
+#reads in the training and testing csvs into lists
 with open('train.csv', 'r') as inf:
     f = csv.reader(inf)
     train = list(f)
 
-#original training dataset converted to np array
+with open('test.csv', 'r') as inf:
+    f = csv.reader(inf)
+    test = list(f)
+
+#original training and testing dataset converted to np array
 o_train = np.array(train)
+o_test = np.array(test)
+
 
 #extracts the text and converts it to a bag of words (frequency), then converts bag of words to a np array
 text = []
@@ -18,6 +24,7 @@ for instance in train[1:]:
 vectorizer = CountVectorizer(stop_words=['Mr.', 'Mrs.','Miss.','Dr.'])
 matrix =  vectorizer.fit_transform(text)
 np_matrix = matrix.toarray()
+#print(np_matrix)
 
 #creates new dataset headers by getting feature names, then maps it to the matrix
 X_col = []
@@ -25,14 +32,19 @@ X_col.append(vectorizer.get_feature_names())
 X_col = np.array(X_col)
 text_train = np.append(X_col, np_matrix, 0)
 
-#removes the name column from the original list as we converted it to bag of words
-for row in train:
-    del row[3]
+#creates the no_text datasets by converting the original training set into an np array then deleting the text column
+train_no_text = np.array(train)
+train_no_text = np.delete(train_no_text, 3, 1)
+test_no_text = np.delete(o_test, 2,1)
 
 #convert the original list into an np array and then combine it with the bag of words
-train_no_text = np.array(train)
 full_train = np.append(train_no_text, text_train, 1)
 
 print(o_train.shape)
+np.savetxt("o_train.csv", o_train, fmt = "%s", delimiter=",")
+print(o_test.shape)
+np.savetxt("test_no_text.csv", test_no_text, fmt = "%s", delimiter=",")
 print(train_no_text.shape)
+np.savetxt("train_no_text.csv", train_no_text, fmt = "%s", delimiter=",")
 print(full_train.shape)
+np.savetxt("full_train.csv", full_train, fmt = "%s", delimiter=",")

169da18bc560345d7674044cea189d6474a7e9b0
Brian Heredia <brianheredia@Brian-Heredia.local>
2018-08-20 16:20:46 -0400

    Altered parameters of the NN to get a better result, currently NN has 96.77% accuracy, need to try convolutional when I get to a computer that has CNTK and keras installed

--- kaggle_mnist/mnist_model.py
+++ kaggle_mnist/mnist_model.py
@@ -8,10 +8,14 @@ import warnings
 
 """
     To Do:
-        1. Build the remaining models and try different parameters, could use gridCV but 
-           not many parameters to try so will just stick with manual tuning
+        1. Try different parameters, could use gridCV but 
+           not many parameters to try so will just stick with manual tuning (done) 
+           Best model:
+                accuracy = .9677
+                layers = input, 512,256,128,64,32, output
+                alpha = 0.0001
 
-        2. Try a convolutional neural network
+        2. Try a convolutional neural network (needs to be done on desktop, no CNTK for mac)
             2.1. Need to convert data into a 28x28x1 format for ConvNet 
             2.2. Build keras ConvNet model
             2.3. Figure out how to import keras model into sklearn, try
@@ -22,41 +26,30 @@ import warnings
 
 #data preparation
 def load_data(training_file, testing_file):
+    #instances consists of 784 features (28x28x1 image if we convert to matrix form)
     train = pd.read_csv(training_file, header = 0)
     test = pd.read_csv(testing_file, header = 0)
+    
     y_train = train['label']
+    train = train.drop('label', axis=1)
     
-    #check class balance in training set
-    #print(train['label'].value_counts())
-
-    #create copies to not alter the originals
-    train_c = train.copy().drop('label', axis=1)
-    test_c = test.copy()
-    
-    return [train_c, test_c], y_train
+    return [train, test], y_train
 
 
 
-#Models
-def MLP_model(n_layers, n_neurons):
-    return  MLPClassifier(hidden_layer_sizes=(n_layers, n_neurons), verbose=True, random_state=1)
-
-def linearSVM():
-    pass
-
-def SVM():
-    pass
-
-def RF():
-    pass
+#Model
+def MLP_model(n_neurons):
+    #random_state is set for reproducibility, sets all weights to the same starting point
+    return  MLPClassifier(hidden_layer_sizes=(n_neurons), verbose=True, random_state=1, alpha=.0001)
 
 
 #predict and get file
-def predict(clf, test_data):
+def predict(clf, test_data, accuracy):
     def create_submission_csv(predicted_labels, test_data):
-        test_data['index'] = test_data['index'] + 1
+        test_data = test_data.reset_index()
+        test_data['index'] += 1
         test_data['Label'] = predicted_labels
-        test_data.to_csv('mnist_predictions_mlp.csv', sep=',', columns=['index', 'Label'], index=False)
+        test_data.to_csv('mnist_predictions_mlp_{0:.{1}f}.csv'.format(accuracy, 4), sep=',', columns=['index', 'Label'], index=False)
 
     predicted_labels = clf.predict(test_data)
     create_submission_csv(predicted_labels, test_data)
@@ -66,11 +59,18 @@ def predict(clf, test_data):
 #driver
 def main():
     datasets, y_train = load_data('train.csv', 'test.csv')
-    mlp = MLP_model(3, 784)
+
+    #hidden layers should be between input and output layer sizes (785 to 10) and 
+    #should only ever decrease or stay the same so you don't lose information
+    mlp = MLP_model((512,256,128,64,32))
     mlp.fit(datasets[0], y_train)
+    
+    #test model using cross-validation, 5-fold should be enough
     scores = cross_val_score(mlp, datasets[0], y_train, cv=5)
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean()*100, (scores.std() *100)))
-    predict(mlp, datasets[1])
+    
+    #predict test instances and create submission file
+    predict(mlp, datasets[1], scores.mean())
 
 
 if __name__ == '__main__':
